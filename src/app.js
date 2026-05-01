fd8b51c979c6e7a627a8e3ce4369da234e0e1222
Dzmitry Navitski <dzmitry.navitski@fanduel.com>
2026-05-01 23:45:06 +0100

    change urls

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import {webhookCallback} from "grammy";
 import {bot} from "./bot.js";
 import homeRouter from "./routes/ugc-free-lesson.js";
-import privacyRouter from "./routes/privacy.js";
 
 const {WEBHOOK_PATH = "/webhook"} = process.env;
 
@@ -10,7 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.use(homeRouter);
-app.use(privacyRouter);
 app.post(WEBHOOK_PATH, webhookCallback(bot, "express"));
 app.get("/health", (_req, res) => res.json({status: "ok"}));
 
